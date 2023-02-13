569448f2954de0bd64af067e6fbf5bbb0d64f4be
MaksimVS <VdovinM.S@mail.ru>
2023-02-13 23:39:25 +0300

    add Выполнили четвертую задачу с выводом четных чисел

---
+++ Task004/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+int N, i;
+Console.WriteLine("Введите положительное число");
+N = Convert.ToInt32(Console.ReadLine());
+i = 0;
+int[] arr = new int[N];
+
+arr[i] = 0;
+while (i < N)
+{
+    arr[i] = i + 1;
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write(arr[i]);
+        Console.Write(" ");
+    }
+    i = i + 1;
+}
+
+
