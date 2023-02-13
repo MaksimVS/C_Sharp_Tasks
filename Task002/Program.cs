ee8962d9417327b77d195c9e9fde867441b98343
MaksimVS <VdovinM.S@mail.ru>
2023-02-13 20:09:26 +0300

    add Решение второй задачи с нахождением максимального из трех чисел

---
+++ Task002/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+int a, b, c, max;
+
+Console.WriteLine("Введите три числа");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+if (a >= b)
+{
+    max = a;
+    if (c >= a)
+    {
+        max = c;
+    }
+}
+else
+{
+    max = b;
+    if (c >= max)
+    {
+        max = c;
+    }
+}
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
+
+
