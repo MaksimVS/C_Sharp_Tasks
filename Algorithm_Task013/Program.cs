bbe940295280f3e31aae76b6834041742794553d
MaksimVS <VdovinM.S@mail.ru>
2023-02-20 01:24:29 +0700

    упаковал программу в два метода

---
+++ Algorithm_Task013/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+
+Console.WriteLine("Введите число!");
+int Value = Convert.ToInt32(Console.ReadLine());
+checkNumber();
+
+void checkNumber()           // Метод проверяет наличие третьей цифры и выдает ее, если находит 
+    {
+        if (Value < 100) 
+                {
+                Console.WriteLine("Третьей цифры нет!");
+                }
+        else
+            {
+            int thirdValue = prunningNumber() % 10;
+            Console.WriteLine("Третья цифра числа = " + thirdValue);
+            }
+    }
+
+int prunningNumber()         // функция(метод) обрезает лишние цифры
+    {
+      while (Value > 999) 
+            {
+             Value = Value / 10;
+            }                 
+     return Value;
+    }  
