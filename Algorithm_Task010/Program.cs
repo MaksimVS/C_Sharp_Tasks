2b2651cde58b4042aad1b9bc8ef15dcd479ba2fd
MaksimVS <VdovinM.S@mail.ru>
2023-02-19 22:17:51 +0700

    переименование одной папки

---
+++ Algorithm_Task010/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// Используем функции (методы)
+
+int enterNumber()         // функция(метод) с приглашением к вводу трехзначного числа
+    {
+        int Value = 0;
+        while (!(Value > 99 && Value < 1000))
+            {
+                Console.WriteLine("Введите трехзначное число!");
+                Value = Convert.ToInt32(Console.ReadLine());      
+            }
+        return Value;
+    }
+
+int secondValue = (enterNumber() / 10) % 10;
+Console.WriteLine("Вторая цифра числа = " + secondValue);
+
+        
+
+ 
