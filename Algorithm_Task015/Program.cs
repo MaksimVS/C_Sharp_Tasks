9d120bc4b8ec8dd909cf1973c1d6c16c14310ddd
MaksimVS <VdovinM.S@mail.ru>
2023-02-20 20:37:33 +0700

    Решение задачи с днями недели

---
+++ Algorithm_Task015/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+bool checkValue ()     // Метод оценивающий введенную, согласно задания, цифру
+    {
+        int value = 0;
+        while (value < 1 || value > 7)
+            {
+                Console.WriteLine("Введите цифру от 1 до 7 обозначающую день недели");
+                value = Convert.ToInt32(Console.ReadLine());
+            }
+        if (value >= 1 && value <= 5) return true;
+        else return false;
+    }
+
+if (checkValue() == true) 
+    {
+        Console.WriteLine("Будничный день..."); 
+    }
+else Console.WriteLine("Выходной день...");    
