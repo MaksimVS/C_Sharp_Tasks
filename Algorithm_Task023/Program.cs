c484fc828f4d3d3e11553fca09a03bae3fe918f4
MaksimVS <VdovinM.S@mail.ru>
2023-02-25 01:07:27 +0700

    Вывод кубов чисел в виде таблицы- надо доработать- отделить таблицу от подсчета куба в отдельный метод и поработать над правильным отображением по столбцам- возможно через массивы

--- Algorithm_Task023/Program.cs
+++ Algorithm_Task023/Program.cs
@@ -2,3 +2,38 @@
 //выдаёт таблицу кубов чисел от 1 до N.
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Cube(N);
+
+void Cube(int N)
+{
+    int column = 0;
+    for (column = 0; column < 10; column++)
+        Console.Write("   " + column);
+    Console.WriteLine();
+    int str = 0;
+    column = 0;
+    Console.Write(str);
+
+    for (int i = 0; i < N; i++, column++)
+    {
+        if (column < 10)
+        {
+            int number = i * i * i;
+            Console.Write("  " + number);        
+        }
+        else 
+        {
+            Console.WriteLine();
+            str++;
+            Console.Write(str);
+            int number = i * i * i;
+            Console.Write("  " + number);
+            column = 0;       
+        }
+    }
+
+}
+
