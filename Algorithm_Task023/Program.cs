a780fa1de7ad3e6fc63c4631f1d4d3f8e973d7e7
MaksimVS <VdovinM.S@mail.ru>
2023-02-25 13:24:54 +0700

    доработал программу

--- Algorithm_Task023/Program.cs
+++ Algorithm_Task023/Program.cs
@@ -1,39 +1,47 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и 
-//выдаёт таблицу кубов чисел от 1 до N.
+//выдаёт таблицу кубов чисел от 1 до N. Сделал при N <= 100, 
+//для использования табличного стиля
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
 
 Console.WriteLine("Введите число");
 int N = Convert.ToInt32(Console.ReadLine());
-Cube(N);
+Console.WriteLine("Таблица кубов чисел от 1 до " + N + ": ");
+int column = 0;
+for (column = 0; column < 10; column++)
+    Console.Write("      " + column);
+Console.WriteLine();
+column = 0;
+int i = 0;
 
-void Cube(int N)
+Construction();
+
+void Construction()
 {
-    int column = 0;
-    for (column = 0; column < 10; column++)
-        Console.Write("   " + column);
-    Console.WriteLine();
     int str = 0;
-    column = 0;
     Console.Write(str);
-
-    for (int i = 0; i < N; i++, column++)
+    for (str = 0, i = 0; str < 10 && i < N; i++, column++)
     {
-        if (column < 10)
-        {
-            int number = i * i * i;
-            Console.Write("  " + number);        
-        }
-        else 
+        if (column < 10) Cube(N);
+        else
         {
             Console.WriteLine();
             str++;
             Console.Write(str);
-            int number = i * i * i;
-            Console.Write("  " + number);
-            column = 0;       
+            Cube(N);
+            column = 0;
         }
     }
-
 }
+void Cube(int N)
+{
+    int number = i * i * i;
+    if (number < 10) Console.Write(" " + "00000" + number);
+    else if (number < 100) Console.Write(" " + "0000" + number);
+    else if (number < 1000) Console.Write(" " + "000" + number);
+    else if (number < 10000) Console.Write(" " + "00" + number);
+    else if (number < 100000) Console.Write(" " + "0" + number);
+    else Console.Write(" " + number);
+}
+
 
