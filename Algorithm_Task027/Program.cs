8edecfa06e33a88a39121aa9d0bb9af7a70f2998
MaksimVS <VdovinM.S@mail.ru>
2023-02-25 15:53:35 +0700

    Задача 027 решена

---
+++ Algorithm_Task027/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Sum(number);
+
+void Sum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int value = number % 10;
+        number = number / 10;
+        sum = sum + value;
+    }
+    Console.WriteLine(sum);
+}
