cc0bd316f0c2e914cf58dfeebdb74375d48a1d6e
MaksimVS <VdovinM.S@mail.ru>
2023-02-25 17:04:51 +0700

    Задача 029 решена

---
+++ Algorithm_Task029/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int N = getNumber("Введите размер массива ");  //задаем размер массива
+int[] numbers = createArray(N);
+
+newRandomArr(numbers);
+
+int[] createArray(int N)
+{
+    return new int[N];
+}
+
+int getNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int newRandomArr(int[] randomArr)
+{
+    Random random = new Random();
+    int newResultRand = 0;
+    for (int i = 0; i < randomArr.Length; i++)
+    {
+        newResultRand = random.Next(10);
+        Console.Write(newResultRand + ", ");
+    }
+    return newResultRand;
+}
