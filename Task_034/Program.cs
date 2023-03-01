a36a32eee90f92dccc6f791721bdd16d37d661e9
MaksimVS <VdovinM.S@mail.ru>
2023-03-01 18:40:24 +0700

    выполнил задачу, но не получилось выделить поиск четных в отдельный метод

---
+++ Task_034/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] createArray(int size)
+{
+    return new int[size];
+}
+
+int getNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void newRandomArr(int[] randomArr)
+{
+    Random random = new Random();
+    int count = 0;
+    int newResultRand = 0;
+    for (int i = 0; i < randomArr.Length; i++)
+    {
+        newResultRand = random.Next(100, 1000);
+        Console.Write(newResultRand + " ");
+        if (newResultRand % 2 == 0) count++;
+    }
+    Console.Write("четных чисел: " + count + " ");
+}
+
+int size = getNumber("Введите размер массива ");
+int[] numbers = createArray(size);
+newRandomArr(numbers);
