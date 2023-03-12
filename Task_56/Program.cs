83f4eeb1375c63303ca81f717c3848ec64404308
MaksimVS <VdovinM.S@mail.ru>
2023-03-12 12:12:18 +0700

    создали квадратный массив

---
+++ Task_56/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и 
+//выдаёт номер строки с наименьшей суммой элементов: 0 строка
+
+void FillPrintTable(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("введите число отображающее количество строк и столбцов ,,квадратного,, массива:  ");
+int variable = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[variable, variable];
+FillPrintTable(numbers);
