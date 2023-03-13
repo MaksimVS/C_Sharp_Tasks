d8d3b90cfae9050b5f443ec9882db302659b3336
MaksimVS <VdovinM.S@mail.ru>
2023-03-13 16:53:59 +0700

    задача решена

--- Task_60/Program.cs
+++ Task_60/Program.cs
@@ -5,25 +5,46 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
-//Создам квадратный массив для простоты ввода размера
+//Создам квадратный массив для простоты ввода размера, при желании, 
+//легко заменить значение variable на три разных числа. 
 
-void FillPrintTable(int[,,] array)
+void FillPrintTable3D(int[,,] array3D)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    int[] array1D = new int[90];
+    int m = 0;
+    for (int i = 0; i < array3D.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < array3D.GetLength(1); j++)
         {
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                array[i, j, k] = new Random().Next(10, 99);
-                Console.Write($"{array[i, j, k]} ");
-            }
             Console.WriteLine();
+            for (int k = 0; k < array3D.GetLength(2);)
+            {
+                array3D[i, j, k] = new Random().Next(10, 100);
+                for (m = 0; m < array1D.Length; m++)
+                {
+                    if (array1D[m] == 0)
+                    {
+                        Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+                        array1D[m] = array3D[i, j, k];
+                        k++;
+                        break;
+                    }
+                    else if (array1D[m] != 0 && array1D[m] == array3D[i, j, k])
+                    {
+                        break;
+                    }
+                }
+            }
         }
     }
 }
 
+
 Console.Write("введите число отображающее количество строк и столбцов ,,квадратного,, массива:  ");
 int variable = Convert.ToInt32(Console.ReadLine());
-int[,,] numbers = new int[variable, variable, variable];
-FillPrintTable(numbers);
+if (variable * variable * variable <= 90)
+{
+    int[,,] numbers = new int[variable, variable, variable];
+    FillPrintTable3D(numbers);
+}
+else Console.WriteLine("Количество элементов массива превышает количество двузначных неповторяющихся чисел (90 элементов)! Уменьшите массив!");
